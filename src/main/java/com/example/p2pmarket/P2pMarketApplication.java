69e3ed9aa94d76c2e716de9cdc29e00185e663b1
ekaterrrinnka <katiymau04@.com>
2026-03-09 20:47:48 +0200

    project for la java

---
+++ src/main/java/com/example/p2pmarket/P2pMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.p2pmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2pMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2pMarketApplication.class, args);
+    }
+}
+
---
+++ src/main/java/lab/p2p/LabP2pApplication.java
@@ -0,0 +1,13 @@
+package lab.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabP2pApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabP2pApplication.class, args);
+    }
+}
+
